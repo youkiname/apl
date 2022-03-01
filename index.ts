0b163bbe909b00411c44f7078f305c016ce268ed
youkiname <youkiname@mail.ru>
2022-03-01 11:07:50 +0300

    update tokenizer. Add comment, loop, extra operators, functions and if statement

--- index.ts
+++ index.ts
@@ -1,12 +1,27 @@
 import { Lexer } from "./lexer";
 
 const code = `
-h = 1 + 2
-h = ,
-fuck = 33
+a = 20
+fun get_hello(name, age) {
+    return 'Hello, ' + name + age
+}
+while (a > 0) {
+    a = a - 1
+    if (a == 2 and a <= 5) {
+        break
+    } else {
+        continue
+    }
+}
+a = a * 5 / 3
+# its my comment
+# really useful comment
+greetings = 'hi!'
 `
 
 const lexer = new Lexer(code)
 
 const tokens = lexer.getTokens()
-console.log(tokens);
+for (let token of tokens) {
+    console.log(token.type.name + ": " + token.value.replace(/\n/, "\\n"));
+}
--- lexer.ts
+++ lexer.ts
@@ -2,23 +2,50 @@
 export class TokenType {
     readonly name: string
     readonly regex: RegExp
+    readonly skip: boolean
 
 
-    constructor(name: string, regex: string) {
+    constructor (name: string, regex: string, skip: boolean = false) {
         this.name = name
         this.regex = new RegExp("^" + regex)
+        this.skip = skip
     }
 }
 
 
 export const tokenTypesList = {
     "NEWLINE": new TokenType("NEWLINE", "\\n"),
-    "SPACE": new TokenType("SPACE", "\\s"),
+    "SPACE": new TokenType("SPACE", "\\s", true),
     "NUMBER": new TokenType("NUMBER", "\\d+"),
+    "COMMA": new TokenType("COMMA", ","),
+    "DOT": new TokenType("DOT", "\\."),
+    "GTE": new TokenType("GTE", ">="),
+    "LTE": new TokenType("LTE", "<="),
+    "GT": new TokenType("GT", ">"),
+    "LT": new TokenType("LT", "<"),
+    "OR": new TokenType("OR", "or"),
+    "AND": new TokenType("OR", "and"),
+    "EQUAL": new TokenType("EQUAL", "=="),
+    "NOT_EQUAL": new TokenType("NOT_EQUAL", "!="),
     "ASSIGN": new TokenType("ASSIGN", "="),
     "PLUS": new TokenType("PLUS", "\\+"),
     "MINUS": new TokenType("MINUS", "-"),
-    "VARIABLE": new TokenType("VARIABLE", "[a-z]+")
+    "STAR": new TokenType("STAR", "\\*"),
+    "SLASH": new TokenType("SLASH", "\/"),
+    "RETURN": new TokenType("RETURN", "return"),
+    "BREAK": new TokenType("BREAK", "break"),
+    "CONTINUE": new TokenType("CONTINUE", "continue"),
+    "WHILE_LOOP": new TokenType("WHILE_LOOP", "while"),
+    "IF_STATEMENT": new TokenType("IF_STATEMENT", "if"),
+    "ELSE_STATEMENT": new TokenType("ELSE_STATEMENT", "else"),
+    "INIT_FUNCTION": new TokenType("INIT_FUNCTION", "fun"),
+    "OPEN_BRACKETS": new TokenType("OPEN_BRACKETS", "\\("),
+    "CLOSE_BRACKETS": new TokenType("CLOSE_BRACKETS", "\\)"),
+    "OPEN_STATEMENT": new TokenType("OPEN_STATEMENT", "{"),
+    "CLOSE_STATEMENT": new TokenType("CLOSE_STATEMENT", "}"),
+    "COMMENT": new TokenType("COMMENT", "#.*\\n", true),
+    "STRING": new TokenType("STRING", "\\'.*\\'"),
+    "VARIABLE": new TokenType("VARIABLE", "[a-z_]+"),
 }
 
 
@@ -26,9 +53,20 @@ export class Token {
     readonly type: TokenType
     readonly value: string
 
-    constructor(type: TokenType, value: string) {
+    constructor (type: TokenType, value: string) {
         this.type = type
         this.value = value
+
+        if (type.name == "STRING") {
+            this.value = this.validateStringValue(value)
+        }
+    }
+
+    public validateStringValue(tokenValue: string): string {
+        if (tokenValue.length == 2) {
+            return tokenValue
+        }
+        return tokenValue.slice(1, tokenValue.length - 1)
     }
 }
 
@@ -46,7 +84,7 @@ export class Lexer {
     private position: number
     private lineNumber: number
 
-    constructor(code: string) {
+    constructor (code: string) {
         this.startCode = code
         this.code = code
         this.tokens = []
@@ -66,7 +104,7 @@ export class Lexer {
             this.code = this.code.substring(match.word.length)
             this.updatePosition(match)
 
-            if (match.type.name != "SPACE") {
+            if (!match.type.skip) {
                 this.tokens.push(new Token(match.type, match.word))
             }
         }
